8be21affa3dd789bc6bcc4a62258d91ec5c85247
Holly Wardlaw <hollywardlaw@Matts-MacBook-Pro.local>
2019-04-04 10:57:12 +0100

    Created a button to load comments

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 class Article extends Component {
   state = {
-    article: {}
+    article: {},
+    comments: []
   };
   render() {
     return (
@@ -12,10 +13,22 @@ class Article extends Component {
         <h4>by {this.state.article.author}</h4>
         <p>Topic: {this.state.article.topic}</p>
         <p>{this.state.article.body}</p>
-        <p>
-          Votes {this.state.article.votes} | Comments{' '}
+        <p>Votes {this.state.article.votes} </p>
+        <button onClick={this.getCommentData}>
+          Comments
           {this.state.article.comment_count}
-        </p>
+        </button>
+        {this.state.comments.length !== 0 &&
+          this.state.comments.map(comment => {
+            return (
+              <div key={comment.comment_id}>
+                <p>Date posted: {comment.created_at.slice(0, 10)}</p>
+                <p>"{comment.body}"</p>
+                <p>by {comment.author}</p>
+                <p>Votes {comment.votes}</p>
+              </div>
+            );
+          })}
       </div>
     );
   }
@@ -29,6 +42,14 @@ class Article extends Component {
 
     axios.get(url).then(res => {
       this.setState({ article: res.data.articles[0] });
+    });
+  };
+  getCommentData = () => {
+    const url = `https://holly-nc-news.herokuapp.com/api/articles/${
+      this.props.article_id
+    }/comments`;
+    axios.get(url).then(res => {
+      this.setState({ comments: res.data });
     });
   };
 }
