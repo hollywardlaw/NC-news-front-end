5cb5cce7bfd4e476a3bef96f19b5ebc511f3a5ff
Holly Wardlaw <hollywardlaw@Matts-MacBook-Pro.local>
2019-05-15 14:58:41 +0100

    deployed

---
+++ build/precache-manifest.5111ed090a86d72ef44d227a9b0b35ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59983b7be76e8770c679e192e870f3e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90a48e02a8f60b3e0526",
+    "url": "/static/css/main.df36afad.chunk.css"
+  },
+  {
+    "revision": "3490b412f0e4d1c6adc6",
+    "url": "/static/js/2.04ee992b.chunk.js"
+  },
+  {
+    "revision": "90a48e02a8f60b3e0526",
+    "url": "/static/js/main.6026d2c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2cca9a6d69e1bb84a424f10ee57c9c01",
+    "url": "/static/media/NCNEWS.2cca9a6d.jpg"
+  }
+]);
